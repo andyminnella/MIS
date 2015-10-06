40b5ad6009649d66b016be8e084825cf6083fa13
andyminnella <andyminnella@gmail.com>
2015-10-05 19:09:43 -0700

    marketing

---
+++ Streamer Story NEW/Assets/Scripts/Andy's Scripts/Marketing script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Marketingscript : MonoBehaviour {
+    //marketing styles
+    public enum marketingTypes
+    {
+        none = 0,
+        basic,
+        gay,
+        someOtherShit,
+        showTits
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
